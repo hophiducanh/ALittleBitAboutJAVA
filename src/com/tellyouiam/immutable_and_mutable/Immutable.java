b12f98b2b9603ea55734e155d5a0ed78852522a5
hophiducanh <contact.hoducanh@gmail.com>
2019-04-14 11:57:07 +0700

    Mutable and immutable in java and package convention

---
+++ src/com/tellyouiam/immutable_and_mutable/Immutable.java
@@ -0,0 +1,29 @@
+package com.tellyouiam.immutable_and_mutable;
+
+public class Immutable {
+    //String is part of java.lang thus we don't need import to use
+    //Initialize str1 varibale = "first"
+    String str1 = new String("first");
+
+    //Initialize str2 reference to str1
+    String str2 = str1;
+
+    //String is immutable, any manipulation on String as well as create new object
+    //str1.concat("-second") will return a new String object which value is "first-second"
+    {
+        str1 = str1.concat("-second");
+        System.out.println("str1: "+str1);
+        System.out.println("str2: "+str2);
+    }
+    //The only way to call system.out method without main method is
+    //declare in code block. But we can't execute this method. huhu
+
+    public static void main(String[] args) {
+        String str1 = new String("first");
+        String str2 = str1;
+
+        str1 = str1.concat("-second");
+        System.out.println("str1: "+str1);
+        System.out.println("str2: "+str2);
+    }
+}
