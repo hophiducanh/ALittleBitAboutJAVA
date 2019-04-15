32c1251296fb2a97e5cbeb5096add60913f327b8
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 01:07:38 +0700

    add static nested class example

--- src/com/tellyouiam/innerclasses/methodlocal/OuterClass.java
+++ src/com/tellyouiam/innerclasses/methodlocal/OuterClass.java
@@ -1,9 +1,13 @@
 package com.tellyouiam.innerclasses.methodlocal;
 
 public class OuterClass {
+
+    //static nested class
+    static class A {}
+
     public void display() {
         int num = 23;
-        class Inner{
+         class Inner{
             //method local inner class
             public void print() {
                 System.out.println("This is method inner class " + num);
