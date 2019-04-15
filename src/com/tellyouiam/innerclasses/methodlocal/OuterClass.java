9b6ac685f8a5c206a20537bd8855ebc785d1a180
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 01:05:41 +0700

    method local of inner class

---
+++ src/com/tellyouiam/innerclasses/methodlocal/OuterClass.java
@@ -0,0 +1,20 @@
+package com.tellyouiam.innerclasses.methodlocal;
+
+public class OuterClass {
+    public void display() {
+        int num = 23;
+        class Inner{
+            //method local inner class
+            public void print() {
+                System.out.println("This is method inner class " + num);
+            }
+        }
+        Inner obj = new Inner();
+        obj.print();
+    }
+
+    public static void main(String[] args) {
+        OuterClass outerClass = new OuterClass();
+        outerClass.display();
+    }
+}
