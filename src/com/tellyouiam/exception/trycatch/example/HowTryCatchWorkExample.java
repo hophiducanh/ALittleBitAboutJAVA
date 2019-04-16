beef739af1689408ddd47dbec72caad92af67a85
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 20:48:26 +0700

    add detail how try catch work

--- src/com/tellyouiam/exception/trycatch/example/HowTryCatchWorkExample.java
+++ src/com/tellyouiam/exception/trycatch/example/HowTryCatchWorkExample.java
@@ -3,6 +3,13 @@ package com.tellyouiam.exception.trycatch.example;
 public class HowTryCatchWorkExample {
     public static void main(String[] args) {
 
+        /**
+         *  Không phải lúc nào các câu lệnh bên trong khối try cũng gây ra Exception. Nếu câu lệnh trong khối
+         *  try không gây ra exception thì catch sẽ không được thực thi.
+         * */
+
+        //Try là nơi để bao lấy đoạn code có khả năng gây ra exception
+        //Try is a place to cover code that is likely to cause an exception
         try {
             System.out.println("Problem of division");
 
