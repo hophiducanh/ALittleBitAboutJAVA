8ae255aa0eb9dd22577538850bb11bf7756ba19a
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 20:26:11 +0700

    add demo try catch java

---
+++ src/com/tellyouiam/exception/trycatch/example/Example1.java
@@ -0,0 +1,22 @@
+package com.tellyouiam.exception.trycatch.example;
+
+public class Example1 {
+    public static void main(String[] args) {
+        //This code fall into UncheckedException, the compiler don't recognize and check whether Exception has occur
+
+        //        int[] num = {1, 2, 3, 4};
+        //        System.out.println(num[5]);
+
+        //Nhưng nếu trình biên dịch không làm gì cả thì chúng ta cũng đừng đứng khoanh tay nhé.
+        // Để chắc chắn ứng dụng của chúng ta “khỏe mạnh”, bạn nên lường trước các tình huống có khả năng xảy ra Exception
+        // và bao các đoạn code “có khả năng gây bệnh” vào khối try catch.
+
+        try {
+            int[] num = {1, 2, 3, 4};
+            System.out.println(num[5]);
+            //Chạy thử đọc lỗi rồi lấy lỗi đó bỏ vào catch nhé.
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("can't find num[5] element in this array");
+        }
+    }
+}
