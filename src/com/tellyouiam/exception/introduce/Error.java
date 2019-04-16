98940143ce37291971163ec8ba0c9cbcd5d115be
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 22:13:17 +0700

    Add docs for handle exception

---
+++ src/com/tellyouiam/exception/introduce/Error.java
@@ -0,0 +1,7 @@
+package com.tellyouiam.exception.introduce;
+
+public class Error {
+    //Không giống exception, nhưng vấn đề xảy ra vượt quá tầm kiểm soát của lập trình viên hay người dùng.
+    // Error được bỏ qua trong code của bạn vì bạn hiếm khi có thể làm gì đó khi chương trình bị error.
+    // Ví dụ như OutOfMemoryError, VirtualMachineError, AssertionError, …
+}
