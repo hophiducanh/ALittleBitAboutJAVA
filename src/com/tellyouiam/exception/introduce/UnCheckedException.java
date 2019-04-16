48c3089b826f945bcde62fb7cb4ae6c63307c960
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 17:12:20 +0700

    add unchecked exception

---
+++ src/com/tellyouiam/exception/introduce/UnCheckedException.java
@@ -0,0 +1,13 @@
+package com.tellyouiam.exception.introduce;
+
+public class UnCheckedException {
+
+    //Exception này khá nguy hiểm và xảy ra khi ứng dụng đang thực thi nên được gọi là RUNTIME EXCEPTION
+
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4};
+
+        //Mảng không có phần tử thứ 5
+        System.out.println(num[5]);
+    }
+}
