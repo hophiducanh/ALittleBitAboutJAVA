cf3f1129b93d785e772fcd845c801b05657e52d9
hophiducanh <contact.hoducanh@gmail.com>
2019-04-16 17:37:29 +0700

    practice exercise checked and unchecked exception

---
+++ src/com/tellyouiam/exception/introduce/practice/Practice1.java
@@ -0,0 +1,16 @@
+package com.tellyouiam.exception.introduce.practice;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        int i = Integer.parseInt("10");
+
+        //Ctrl + B go to declaration will see:
+        //public static float parseFloat(String s) throws NumberFormatException ---> Ctrl + B to see declaration of NumberFormatException --->
+        //IllegalArgumentException ----> RuntimeException (Hence i have conclusion: this is a runtime exception or unchecked exception)
+        float f = Float.parseFloat("4.5a");
+
+        System.out.println(i);
+        System.out.println(f);
+    }
+}
