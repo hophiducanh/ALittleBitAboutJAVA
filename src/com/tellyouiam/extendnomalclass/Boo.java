fba9bc20f9e1246e5bb581ddc5612f3e6d69f8ea
hophiducanh <contact.hoducanh@gmail.com>
2019-04-17 11:29:15 +0700

    add normal class extends normal class

---
+++ src/com/tellyouiam/extendnomalclass/Boo.java
@@ -0,0 +1,9 @@
+package com.tellyouiam.extendnomalclass;
+
+public class Boo extends Foo {
+
+    void A() {
+        System.out.println(a);
+    }
+
+}
---
+++ src/com/tellyouiam/extendnomalclass/Foo.java
@@ -0,0 +1,9 @@
+package com.tellyouiam.extendnomalclass;
+
+class Foo {
+    //some code here
+     int a;
+    void A () {
+        System.out.println("AA");
+    }
+}
