6903805247733e1abc60affc3d63b54e1c2aafa1
hophiducanh <contact.hoducanh@gmail.com>
2019-04-18 21:58:09 +0700

    update docs multiple inheritance interface

--- src/com/tellyouiam/interfaces/multiple/inheritance/InterfaceA.java
+++ src/com/tellyouiam/interfaces/multiple/inheritance/InterfaceA.java
@@ -1,5 +1,10 @@
 package com.tellyouiam.interfaces.multiple.inheritance;
 
+/**
+ * Java không cho phép đa kế thừa vì vấn đề (trong docs) Diamond problem (class) nhưng đối với interface thì java có hỗ trợ
+ * cơ chế xử lý vấn đề này
+ * Reference: http://codeinventions.blogspot.com/2014/07/can-interface-extend-multiple.html
+ **/
 public interface InterfaceA {
     void testMethod();
     void A();
