06f6b75398d857e67f77bfb3d19eaa9626f7ff5c
hophiducanh <contact.hoducanh@gmail.com>
2019-04-30 01:38:38 +0700

    add primitive type literal

---
+++ src/com/tellyouiam/literals/assignments/variables/primitivetypes/booleanliterals/BooleanLiteral.java
@@ -0,0 +1,10 @@
+package com.tellyouiam.literals.assignments.variables.primitivetypes.booleanliterals;
+/**
+ * Boolean literals are the source code representation for boolean values. A boolean value can be defined
+ * only as true or false. Although in C (and some other languages), it is common to use numbers to
+ * represent true or false, this will not work in Java. Again, repeat after me: “Java is not C.”
+ * */
+public class BooleanLiteral {
+    boolean t = true; //Legal
+    //boolean f = 0;  //Compiler error!
+}
