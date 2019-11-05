c58ba7569ce3c8fbde69bd130af355f03cfa6a07
hophiducanh <contact.hoducanh@gmail.com>
2019-11-05 14:16:12 +0700

    Add practical exercise for iterator().next() method in java

---
+++ src/com/tellyouiam/collections/iterator/NextMethod.java
@@ -0,0 +1,24 @@
+package com.tellyouiam.collections.iterator;
+
+import java.util.*;
+
+public class NextMethod {
+	public static void main(String[] args) {
+		ArrayList<Integer> ls = new ArrayList<>(1);
+		//ls = new LinkedList<>();
+		/*can't add more in fixed size ArrayList*/
+//		ls.addAll(1, Collections.singleton(2));
+//		ls.forEach(System.out::println);
+		
+		List<Integer> lss = new ArrayList<>(1);
+		lss = new LinkedList<>();
+		
+		/**
+		 * Object next(): It returns the next element in the collection until the hasNext()method return true.
+		 * This method throws ‘NoSuchElementException’ if there is no next element.
+		 * */
+		
+		List<Integer> list = Arrays.asList(1, 2);
+		System.out.println(list.iterator().next()); //1 >> GET THE FIRST ONE
+	}
+}
