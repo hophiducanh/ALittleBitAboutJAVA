0b66c6854e598050ddba2d6aafe05600eecbbe1f
hophiducanh <contact.hoducanh@gmail.com>
2020-03-13 18:08:07 +0700

    learn something

--- src/com/tellyouiam/file/io/csv/opencsv/Main.java
+++ src/com/tellyouiam/file/io/csv/opencsv/Main.java
@@ -3,17 +3,27 @@ package com.tellyouiam.file.io.csv.opencsv;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.List;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
-	public static void main(String[] args) {
-		try {
-			List<Object> beans = new CsvToBeanBuilder<>(new FileReader("yourfile.csv"))
-					.withType(Visitors.class).build().parse();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+	
+	private static final String FOLDER_PATH = "C:\\Users\\conta\\OneDrive\\Desktop\\data\\test-data\\";
+	
+	public static void main(String[] args) throws IOException {
+		try (
+				Reader reader = Files.newBufferedReader(Paths.get(FOLDER_PATH.concat("opencsv-columnname.csv")));
+		) {
+			CsvToBean<Visitors> csvToBean = new CsvToBeanBuilder(reader)
+					.withType(Visitors.class)
+					.withIgnoreLeadingWhiteSpace(true)
+					.build();
+			
+			for (Visitors csvUser : csvToBean) {
+				System.out.println(csvUser);
+			}
 		}
 	}
 }
