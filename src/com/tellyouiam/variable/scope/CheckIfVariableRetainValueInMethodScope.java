6c12a4c11f6aa378926e87c97f56f2c4c7dd2b81
hophiducanh <contact.hoducanh@gmail.com>
2020-05-05 18:10:03 +0700

    collection retains value after method call

--- src/com/tellyouiam/variable/scope/CheckIfVariableRetainValueInMethodScope.java
+++ src/com/tellyouiam/variable/scope/CheckIfVariableRetainValueInMethodScope.java
@@ -1,14 +1,26 @@
 package com.tellyouiam.variable.scope;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CheckIfVariableRetainValueInMethodScope {
 	private static void alter(String name) {
 		name = "lala";
 	}
 	
+	private static void add(List<String> x) {
+		x.addAll(Arrays.asList("a", "b"));
+	}
+	
 	public static void main(String[] args) {
 		String name = "Logbasex";
+		List<String> x = new ArrayList<>();
 		
 		alter(name);
 		System.out.println(name);//logbasex
+		
+		add(x);
+		System.out.println(x.size()); //2
 	}
 }
